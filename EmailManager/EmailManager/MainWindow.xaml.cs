1b6933e5820bbc090b2da54cb94144be27c74a74
tomaMin <tamara.minaeva@firstlinesoftware.com>
2019-08-12 11:55:22 +0300

    add pages on windows

--- EmailManager/EmailManager/MainWindow.xaml.cs
+++ EmailManager/EmailManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 using EmailManager.ViewModels;
 
@@ -18,14 +19,14 @@ namespace EmailManager
 
         private void BtnOpenMenu_Click(object sender, RoutedEventArgs e)
         {
-            btnOpenMenu.Visibility = Visibility.Collapsed;
-            btnCloseMenu.Visibility = Visibility.Visible;
+            BtnOpenMenu.Visibility = Visibility.Collapsed;
+            BtnCloseMenu.Visibility = Visibility.Visible;
         }
 
         private void BtnCloseMenu_Click(object sender, RoutedEventArgs e)
         {
-            btnOpenMenu.Visibility = Visibility.Visible;
-            btnCloseMenu.Visibility = Visibility.Collapsed;
+            BtnOpenMenu.Visibility = Visibility.Visible;
+            BtnCloseMenu.Visibility = Visibility.Collapsed;
         }
 
         private void BtnCloseWindow_Click(object sender, RoutedEventArgs e)
@@ -42,6 +43,22 @@ namespace EmailManager
         private void BtnMinimizeWindow_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+        private void CreateItem_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            FramePage.NavigationService.Navigate(new Uri("View/CreateEmailPage.xaml", UriKind.Relative));
+        }
+
+        private void SearchItem_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            FramePage.NavigationService.Navigate(new Uri("View/SearchEmailsPage.xaml", UriKind.Relative));
+        }
+
+        private void GridFrame_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            BtnOpenMenu.Visibility = Visibility.Visible;
+            BtnCloseMenu.Visibility = Visibility.Collapsed;
         }
     }
 }
---
+++ EmailManager/EmailManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailManager.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public bool IsOpened { get; set; }
+    }
+}
