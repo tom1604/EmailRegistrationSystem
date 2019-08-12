2632584a674940248eabb4a64ca335fe47f813d3
tomaMin <tamara.minaeva@firstlinesoftware.com>
2019-08-12 09:17:39 +0300

    Revert "Revert "Add first Navigation menu""
    This reverts commit e31d5068a5868201fb91aa984b939bb155023ea8.

--- EmailManager/EmailManager/MainWindow.xaml.cs
+++ EmailManager/EmailManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using EmailManager.ViewModels;
 
 namespace EmailManager
@@ -13,6 +14,34 @@ namespace EmailManager
             InitializeComponent();
 
             DataContext = new ListEmailsViewModel();
+        }
+
+        private void BtnOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            btnOpenMenu.Visibility = Visibility.Collapsed;
+            btnCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void BtnCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            btnOpenMenu.Visibility = Visibility.Visible;
+            btnCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnCloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void GridTopPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void BtnMinimizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
