31b34a5430f15a377d22623503d91fe3f52cb693
tomaMin <tamara.minaeva@firstlinesoftware.com>
2019-09-18 17:39:52 +0300

    change binding add button

--- EmailManager/EmailManager/View/CreateEmailPage.xaml.cs
+++ EmailManager/EmailManager/View/CreateEmailPage.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using EmailManager.ViewModels;
 
 namespace EmailManager.View
 {
     /// <summary>
     /// Interaction logic for Page1.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class CreateEmailPage : Page
     {
-        public Page1()
+        public CreateEmailPage()
         {
             InitializeComponent();
+
+            this.DataContext = new CreateEmailViewModel();
         }
     }
 }
---
+++ EmailManager/EmailManager/ViewModels/CreateEmailViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using EmailManager.Models;
+
+namespace EmailManager.ViewModels
+{
+    public class CreateEmailViewModel : INotifyPropertyChanged
+    {
+
+        public CreateEmailViewModel()
+        {
+            Email = new Email();
+        }
+        private Email _email;
+
+        public Email Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                //check valid
+                OnPropertyChanged("Email");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
