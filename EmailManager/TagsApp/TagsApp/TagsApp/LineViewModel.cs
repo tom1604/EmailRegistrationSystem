23e6b210c7648f33ddbd7448e049cc0e9a1177b0
Kant5 <m-cat90@yandex.ru>
2020-02-09 23:16:07 +0300

    add project with add tegs

---
+++ EmailManager/TagsApp/TagsApp/TagsApp/LineViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TagsApp
+{
+    public class LineViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Line> Lines { get; set; } = new ObservableCollection<Line> { new Line { Title = "sdfsdf" }, new Line { Title = "sdfsdf" } };
+        private RelayCommand addCommand;
+        private RelayCommand deleteCommand;
+
+        private Line enteredLine;
+
+        public Line EnteredLine
+        {
+            get { return enteredLine; }
+            set
+            {
+                enteredLine = value;
+                OnPropertyChanged("EnteredLine");
+            }
+        }
+
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ?? (deleteCommand = new RelayCommand(obj =>
+                {
+                    DeleteLine((Line)obj);
+                }));
+            }
+        }
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(obj =>
+                  {
+                      Lines.Insert(0, new Line { Title = (string) obj });
+                }));
+            }
+        }
+
+        private void DeleteLine(Line obj)
+        {
+            if(obj != null && Lines != null)
+            {
+                Lines.Remove(obj);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
