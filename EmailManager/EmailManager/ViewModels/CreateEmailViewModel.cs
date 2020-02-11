bbf21509d3590bc09d1e38a465534b7be062b590
tomaMin <tamara.minaeva@firstlinesoftware.com>
2020-02-11 15:49:09 +0300

    add Tegs when create email

--- EmailManager/EmailManager/ViewModels/CreateEmailViewModel.cs
+++ EmailManager/EmailManager/ViewModels/CreateEmailViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using EmailManager.Models;
 
@@ -6,12 +7,29 @@ namespace EmailManager.ViewModels
 {
     public class CreateEmailViewModel : INotifyPropertyChanged
     {
+        public ObservableCollection<Teg> Tegs { get; set; }
+
+        private RelayCommand _addCommand;
+        private RelayCommand _deleteCommand;
+
+        private string _enteredTegText;
+        private Email _email;
 
         public CreateEmailViewModel()
         {
             Email = new Email();
+            Tegs = new ObservableCollection<Teg>();
         }
-        private Email _email;
+
+        public string EnteredTegText
+        {
+            get => _enteredTegText;
+            set
+            {
+                _enteredTegText = value;
+                OnPropertyChanged(nameof(EnteredTegText));
+            }
+        }
 
         public Email Email
         {
@@ -20,10 +38,52 @@ namespace EmailManager.ViewModels
             {
                 _email = value;
                 //check valid
-                OnPropertyChanged("Email");
+                OnPropertyChanged(nameof(Email));
             }
         }
 
+
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return _deleteCommand ?? (_deleteCommand = new RelayCommand(obj =>
+                {
+                    DeleteLine((Teg)obj);
+                }));
+            }
+        }
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return _addCommand ?? (_addCommand = new RelayCommand(obj =>
+                {
+                    var teg = obj as string;
+                    if (string.IsNullOrWhiteSpace(teg))
+                    {
+                        return;
+                    }
+
+                    CreateTeg(teg);
+                    EnteredTegText = string.Empty;
+                }));
+            }
+        }
+
+        private void CreateTeg(string value)
+        {
+            Tegs.Add(new Teg{Name = value});
+        }
+
+        private void DeleteLine(Teg teg)
+        {
+            if (teg != null)
+            {
+                Tegs.Remove(teg);
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
